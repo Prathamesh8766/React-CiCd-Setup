eb9e2f188745ba041d81abcba9756907a5304bf8
Prathamesh8766 <prathampatil123789@gmail.com>
2025-03-13 10:33:18 +0000

    add lint

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { mergeConfig } from 'vite';
+
 import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
